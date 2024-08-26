118d53f487220edee8d9605cefdd1db51a1b9ae3
Anabil Debnath <73880095+anabildebnath@users.noreply.github.com>
2024-08-26 10:29:40 +0600

    pie chart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Component } from "./components/ui/Component";
 import { DatePickerForm } from "./components/ui/DatePickerForm";
 
 function App() {
   return (
     <div>
       <DatePickerForm />
+      <div className="mt-12">
+          <Component/>
+      </div>
+    
     </div>
   );
 }
